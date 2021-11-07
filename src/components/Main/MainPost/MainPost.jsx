fc2c3153798bfa473dfdcbc92783ed0bbdb813d2
Truong99 <leanhtruong19992017@gmail.com>
2021-11-07 16:28:05 +0700

    render list post, context, transmission props

--- src/components/Main/MainPost/MainPost.jsx
+++ src/components/Main/MainPost/MainPost.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import MainAddPost from './MainAddPost/MainAddPost';
 import MainPostContent from './MainPostContent/MainPostContent';
+import { Consumer } from '../../../Data'; 
 
 class MainPost extends Component {
     render() {
         return (
             <div className="main--post">
                 <MainAddPost/>
-                <MainPostContent/>
-                <MainPostContent/>
-                <MainPostContent/>
-                <MainPostContent/>
+                <Consumer>
+                    {value => {
+                        const { post } = value;
+                        return(
+                            <React.Fragment>
+                                {post.map((item, index) => {
+                                    return (
+                                    <MainPostContent
+                                        key={index}
+                                        data={item}
+                                    />)
+                                })}
+                            </React.Fragment>
+                        )
+                    }}
+                </Consumer>
             </div>
         );
     }
