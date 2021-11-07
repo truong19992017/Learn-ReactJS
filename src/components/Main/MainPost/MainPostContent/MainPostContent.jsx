42891c6e4e14ab2385fad39fd39b8442e4b6b2dd
Truong99 <leanhtruong19992017@gmail.com>
2021-11-07 16:28:26 +0700

    take props

--- src/components/Main/MainPost/MainPostContent/MainPostContent.jsx
+++ src/components/Main/MainPost/MainPostContent/MainPostContent.jsx
@@ -2,21 +2,19 @@ import React, { Component } from 'react';
 
 class MainPostContent extends Component {
     render() {
+        const { name, avatarImg, date, content,
+                img, sumLike, sumCmt } = this.props.data;
         return (
             <div className="main--post--content">
                 <div className="post--avatar">
-                    <img src="https://phunugioi.com/wp-content/uploads/2020/01/anh-avatar-supreme-dep-lam-dai-dien-facebook.jpg" alt="" />
+                    <img src={avatarImg} alt="" />
                 </div>
                 <div className="post--content">
-                    <a>Lê Anh Trường</a>
-                    <h5>21:16 21/12/2011</h5>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Quos accusamus illum nihil perspiciatis eaque et possimus impedit,
-                        architecto quisquam excepturi qui deleniti illo explicabo,
-                        voluptate sit ipsam vitae cum sequi?
-                    </p>
+                    <a>{name}</a>
+                    <h5>{date}</h5>
+                    <p>{content}</p>
                     <div className="post--img">
-                        <img src="https://i.pinimg.com/564x/ce/12/25/ce1225f92e766b3a87113dc69560e88f.jpg" alt="" />
+                        <img src={img} alt="" />
                     </div>
                 </div>
                 <div className="post--action">
@@ -24,13 +22,13 @@ class MainPostContent extends Component {
                         <div className="post--action--like">
                             <i className="fas fa-heart" />
                         </div>
-                        <h5>20k</h5>
+                        <h5>{sumLike}K</h5>
                     </div>
                     <div className="post--action2">
                         <div className="post--action--comment">
                             <i className="fas fa-comment-dots" />
                         </div>
-                        <h5>50k</h5>
+                        <h5>{sumCmt}K</h5>
                     </div>
                 </div>
             </div>
