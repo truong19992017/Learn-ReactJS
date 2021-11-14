c5a85956ea403912168e425f42feffdaba666966
Truong99 <leanhtruong19992017@gmail.com>
2021-11-14 15:35:33 +0700

    add component info

---
+++ src/components/Info.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+    render() {
+        return (
+            <div className="main">
+                <h2>Info page</h2>
+            </div>
+        );
+    }
+}
+
+export default Info;
