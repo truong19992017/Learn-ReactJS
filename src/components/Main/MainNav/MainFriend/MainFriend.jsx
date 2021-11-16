9ace6309d45e985329ad21c9f3c2145a453fea1a
Truong99 <leanhtruong19992017@gmail.com>
2021-11-16 12:44:33 +0700

    update element Link

--- src/components/Main/MainNav/MainFriend/MainFriend.jsx
+++ src/components/Main/MainNav/MainFriend/MainFriend.jsx
@@ -6,17 +6,17 @@ class MainFriend extends Component {
         return (
             <div className="main--nav--friend">
                 <h3 className="main--nav--friend--title">danh sách bạn</h3>
-                <Link className="main--nav--friend--content">
+                <Link to="/" className="main--nav--friend--content">
                     <div className="main--nav--friend--content--img">
                         <img src="https://phunugioi.com/wp-content/uploads/2020/01/anh-avatar-supreme-dep-lam-dai-dien-facebook.jpg"/></div>
                     <h2>Lê Văn Tèo</h2>
                 </Link>
-                <Link className="main--nav--friend--content">
+                <Link to="/" className="main--nav--friend--content">
                     <div className="main--nav--friend--content--img">
                         <img src="https://phunugioi.com/wp-content/uploads/2020/01/anh-avatar-supreme-dep-lam-dai-dien-facebook.jpg"/></div>
                     <h2>Lê Văn Tèo</h2>
                 </Link>
-                <Link className="main--nav--friend--content">
+                <Link to="/" className="main--nav--friend--content">
                     <div className="main--nav--friend--content--img">
                         <img src="https://phunugioi.com/wp-content/uploads/2020/01/anh-avatar-supreme-dep-lam-dai-dien-facebook.jpg"/></div>
                     <h2>Lê Văn Tèo</h2>
